0f31a3f38bb720da59fab3dd62ffe85a4c58517a
paulpak58 <paulpak58@gmail.com>
2023-04-09 20:12:14 -0500

    scripts v1

---
+++ scripts/fqe/fqe_maze2d_medium_dapg_10.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+home=/Users/paul/Downloads/data_augmented_ope
+D4RL_SUPPRESS_IMPORT_ERROR=1 python ${home}/src/data_augmented_train_eval.py \
+    --logtostderr \
+    --d4rl \
+    --env_name=antmaze-medium-play-v2 \
+    --d4rl_policy_filename=./d4rl/antmaze_medium/antmaze_medium_dapg_0.pkl \
+    --target_policy_std=0.0 \
+    --num_mc_episodes=25 \
+    --nobootstrap \
+    --algo=fqe \
+    --noise_scale=0.0 \
+    --num_updates=10000
