dd7cee61343c0edc8039782f0df249a0f45c9c2c
paulpak58 <paulpak58@gmail.com>
2023-04-10 07:33:52 -0500

    updated scripts and requirements for chtc

--- scripts/fqe/fqe_antmaze_umaze_dapg_10.sh
+++ scripts/fqe/fqe_antmaze_umaze_dapg_10.sh
@@ -4,7 +4,7 @@ home=/Users/paul/Downloads/data_augmented_ope
 D4RL_SUPPRESS_IMPORT_ERROR=1 python ${home}/src/data_augmented_train_eval.py \
     --logtostderr \
     --d4rl \
-    --env_name=antmaze-umaze-v2 \
+    --env_name=antmaze-umaze-v1 \
     --d4rl_policy_filename=${home}/src/d4rl/antmaze_umaze/antmaze_umaze_dapg_10.pkl \
     --target_policy_std=0.0 \
     --num_mc_episodes=25 \
