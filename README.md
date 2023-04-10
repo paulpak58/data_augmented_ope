f27f55b88966a730563b39da517b9240cb432547
paulpak58 <paulpak58@gmail.com>
2023-04-09 23:46:27 -0500

    Scripts for FQE

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Data Augmented Off-Policy Evaluation
 Data Augmentation for Off-Policy Evaluation Strategies in Deep Reinforcement Learning
+
+1. Change $D4RL_DATASET_DIR to the dataset directory
+```
+cd src
+sh ../scripts/fqe/fqe_antmaze_umaze_dapg_10.sh
+```
+
--- scripts/fqe/fqe_antmaze_umaze_dapg_5.sh
+++ scripts/fqe/fqe_antmaze_umaze_dapg_5.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 home=/Users/paul/Downloads/data_augmented_ope
-D4RL_SUPPRESS_IMPORT_ERROR=1 python ${home}/augment/data_augmented_train_eval.py \
+D4RL_SUPPRESS_IMPORT_ERROR=1 python ${home}/src/data_augmented_train_eval.py \
     --logtostderr \
     --d4rl \
-    --env_name=antmaze-medium-play-v2 \
-    --d4rl_policy_filename=./d4rl/antmaze_medium/antmaze_medium_dapg_0.pkl \
+    --env_name=antmaze-umaze-v2 \
+    --d4rl_policy_filename=${home}/src/d4rl/antmaze_umaze/antmaze_umaze_dapg_5.pkl \
     --target_policy_std=0.0 \
     --num_mc_episodes=25 \
     --nobootstrap \
     --algo=fqe \
     --noise_scale=0.0 \
-    --num_updates=10000
+    --num_updates=10000 \
+    --save_dir=${home}/results/antmaze_umaze_dapg_5/
